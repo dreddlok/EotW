ad278d36dd5a2f10f68c93f963452dd5345ec414
dreddlok <co.omusi@gmail.com>
2018-04-21 16:50:31 +0100

    simple survivor selection and zombie movement

---
+++ Assets/Survivor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Survivor : MonoBehaviour {
+
+	public float speed = 3;
+    public int health = 5;
+    public int strength = 4;
+    public string pawnName = "Survivor";
+    public Vector3 targetPosition;
+
+    public bool selected;
+
+    public Sprite normalSprite;
+    public Sprite selectedSprite;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.z = -5;
+            if (selected)
+            {
+                GetComponent<SpriteRenderer>().sprite = selectedSprite;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().sprite = normalSprite;
+            }
+        }
+
+        if (selected)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        selected = !selected;
+    }
+}
---
+++ Assets/zombie.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class zombie : MonoBehaviour {
+
+    public float moveTime;
+    public float minMoveTime;
+    public float maxMoveTime;
+    public float waitTime;
+    public float minWaitTime;
+    public float maxWaitTime;
+    public Vector3 moveDirection;
+    // Use this for initialization
+
+	void Start () {
+        moveTime = Random.Range(minMoveTime, maxMoveTime);
+        moveDirection = Random.insideUnitCircle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (moveTime > 0 )
+        {
+            transform.position += moveDirection * Time.deltaTime;
+            moveTime -= Time.deltaTime * 1;
+        }
+        else 
+        {
+            if (waitTime <= 0)
+            {
+                moveTime = Random.Range(minMoveTime, maxMoveTime);
+                moveDirection = Random.insideUnitCircle;
+                waitTime = Random.Range(minWaitTime, maxWaitTime);
+            }
+            else
+            {
+                waitTime -= Time.deltaTime * 1;
+            }
+        }
+	}
+}
